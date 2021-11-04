18fedaec6c7cee707e04c8f2e023f038ab6d7603
Vazovsky <hecchuck17@gmail.com>
2021-11-04 10:56:58 +0400

    Создан урок 6

---
+++ Lesson_6_Klyueva/app/src/main/java/app/vazovsky/lesson_6_klyueva/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.vazovsky.lesson_6_klyueva
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
