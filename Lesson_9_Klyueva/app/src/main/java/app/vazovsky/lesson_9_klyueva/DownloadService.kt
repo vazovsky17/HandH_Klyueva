9fc1d3b953754ab02039d0dd6b37eaa1d0b8a943
vazovsky <46650184+vazovsky17@users.noreply.github.com>
2021-11-21 17:53:44 +0400

    Скачивание, разархивирование и установка картинки

--- Lesson_9_Klyueva/app/src/main/java/app/vazovsky/lesson_9_klyueva/DownloadService.kt
+++ Lesson_9_Klyueva/app/src/main/java/app/vazovsky/lesson_9_klyueva/DownloadService.kt
@@ -3,19 +3,23 @@ package app.vazovsky.lesson_9_klyueva
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.os.Environment
 import android.os.IBinder
 import android.util.Log
 import app.vazovsky.lesson_9_klyueva.presentation.MainActivity
 import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.net.URL
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
 
 
 class DownloadService : Service() {
     companion object {
         const val EXTRA_URL = "extra_url"
         const val DATA_PROGRESS = "data_progress"
+        const val EXTRA_IMAGE = "extra_image"
 
         fun createStartIntent(context: Context): Intent {
             return Intent(context, DownloadService::class.java)
@@ -23,41 +27,84 @@ class DownloadService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val urlToDownload = intent?.getStringExtra(EXTRA_URL)
-        val intent = Intent(MainActivity.ACTION)
-        Log.d("LOL", "Download")
         Thread {
-            try {
-                val url = URL(urlToDownload)
-                val connection = url.openConnection()
-                Log.d("LOL", "Connection")
-                connection.connect()
+            val downloadPath = downloadArchive(intent)
+            if (downloadPath == "") {
+                Log.d("LOL", "Все хуйня")
+            }
+            val unzippingPath = unzip(downloadPath)
 
-                val fileLength = connection.contentLength
-                val input = BufferedInputStream(connection.getInputStream())
-                val path = "${Environment.DIRECTORY_DOWNLOADS}/archive.zip"
-                val output = FileOutputStream(path)
-                val data = byteArrayOf(1024.toByte())
-                var total = 0
-                var count: Int
+            val intentImage = Intent(MainActivity.ACTION_IMAGE)
+            intentImage.putExtra(EXTRA_IMAGE, unzippingPath)
+            sendBroadcast(intentImage)
+        }.start()
+        return START_REDELIVER_INTENT
+    }
+
+    private fun downloadArchive(intent: Intent?): String {
+        val urlToDownload = intent?.getStringExtra(EXTRA_URL)
+        val intentDownload = Intent(MainActivity.ACTION_DOWNLOAD)
+        var path = ""
+        try {
+            val url = URL(urlToDownload)
+            val connection = url.openConnection()
+            connection.connect()
+
+            val fileLength = connection.contentLength
+            val input = BufferedInputStream(connection.getInputStream())
+            val file = File(this.applicationContext.filesDir, "archive.zip")
+            path = file.absolutePath.toString()
+            if (!file.exists()) {
+                file.createNewFile()
+            }
+            val output = FileOutputStream(file)
+            val data = byteArrayOf(1024.toByte())
+            var total = 0
+            var count: Int
+            while (input.read(data).also { count = it } != -1) {
+                total += count
+                intentDownload.putExtra(DATA_PROGRESS, (total * 100 / fileLength))
+                sendBroadcast(intentDownload)
+                output.write(data, 0, count)
+            }
+            output.flush()
+            output.close()
+            input.close()
+
+            intentDownload.putExtra(DATA_PROGRESS, 100)
+            sendBroadcast(intentDownload)
+        } catch (e: Exception) {
+            Log.d("LOL", "что не так: ${e.message}")
+            e.printStackTrace()
+        }
+        return path
+    }
+
+    private fun unzip(downloadPath: String): String {
+        var path = ""
+        try {
+            val input = ZipInputStream(FileInputStream(downloadPath))
+            var entry: ZipEntry
+            val data = byteArrayOf(1024.toByte())
+            var count: Int
+            while (input.nextEntry.also {
+                    entry = it
+                } != null) {
+                val filename = downloadPath + entry.name
+                path = filename
+                val output = FileOutputStream(filename)
                 while (input.read(data).also { count = it } != -1) {
-                    total += count
-                    intent.putExtra(DATA_PROGRESS, (total * 100 / fileLength))
-                    sendBroadcast(intent)
                     output.write(data, 0, count)
                 }
-                output.flush()
                 output.close()
+                input.closeEntry()
                 input.close()
-            } catch (e: Exception) {
 
             }
-
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        intent.putExtra(DATA_PROGRESS, 100)
-        sendBroadcast(intent)
-
-        return START_REDELIVER_INTENT
+        return path
     }
 
     override fun onBind(intent: Intent?): IBinder? {
--- Lesson_9_Klyueva/app/src/main/java/app/vazovsky/lesson_9_klyueva/presentation/MainActivity.kt
+++ Lesson_9_Klyueva/app/src/main/java/app/vazovsky/lesson_9_klyueva/presentation/MainActivity.kt
@@ -6,12 +6,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.ServiceConnection
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import app.vazovsky.lesson_9_klyueva.DownloadService
 import app.vazovsky.lesson_9_klyueva.DownloadService.Companion.DATA_PROGRESS
+import app.vazovsky.lesson_9_klyueva.DownloadService.Companion.EXTRA_IMAGE
 import app.vazovsky.lesson_9_klyueva.DownloadService.Companion.EXTRA_URL
 import app.vazovsky.lesson_9_klyueva.R
 import app.vazovsky.lesson_9_klyueva.ServiceCallbacks
@@ -20,11 +23,13 @@ import app.vazovsky.lesson_9_klyueva.WeatherService.WeatherBinder
 import app.vazovsky.lesson_9_klyueva.data.model.WeatherResponse
 import app.vazovsky.lesson_9_klyueva.databinding.ActivityMainBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity(), ServiceCallbacks {
 
     companion object {
-        const val ACTION = "app.vazovsky.lesson_9_klyueva.TEST_ACTION"
+        const val ACTION_DOWNLOAD = "app.vazovsky.lesson_9_klyueva.ACTION_DOWNLOAD"
+        const val ACTION_IMAGE = "app.vazovsky.lesson_9_klyueva.ACTION_IMAGE"
     }
 
     private val binding by viewBinding(ActivityMainBinding::bind)
@@ -48,17 +53,34 @@ class MainActivity : AppCompatActivity(), ServiceCallbacks {
 
     private val downloadIntent by lazy { DownloadService.createStartIntent(this) }
     private val progressBar by lazy { binding.progressBar }
+    private val imageView by lazy { binding.imageViewPicture }
+    private val buttonStartService by lazy { binding.buttonStartService }
+
     private val downloadReceiver = object : BroadcastReceiver() {
+
         override fun onReceive(context: Context?, intent: Intent?) {
             val progress = intent?.getIntExtra(DATA_PROGRESS, 0) ?: 0
             progressBar.progress = progress
-
+            buttonStartService.text = "$progress %"
             if (progress == 100) {
                 progressBar.visibility = View.GONE
+                buttonStartService.text = "Запуск"
             } else {
                 progressBar.visibility = View.VISIBLE
             }
+        }
 
+    }
+    private val imageReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Log.d("LOL", "Картинка тут ")
+            val path = intent?.getStringExtra(EXTRA_IMAGE)
+            Log.d("LOL", path!!)
+            val imgFile = File(path!!)
+            if (imgFile.exists()) {
+                val bitmap  = BitmapFactory.decodeFile(imgFile.absolutePath)
+                imageView.setImageBitmap(bitmap)
+            }
         }
 
     }
@@ -72,10 +94,12 @@ class MainActivity : AppCompatActivity(), ServiceCallbacks {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding.buttonStartService.setOnClickListener {
-            registerReceiver(downloadReceiver, IntentFilter(ACTION))
+            registerReceiver(downloadReceiver, IntentFilter(ACTION_DOWNLOAD))
+            registerReceiver(imageReceiver, IntentFilter(ACTION_IMAGE))
             progressBar.visibility = View.GONE
+            progressBar.progress = 0
             downloadIntent.apply {
-                putExtra(EXTRA_URL, "https://wdfiles.ru/2833a2")
+                putExtra(EXTRA_URL, "https://vk.com/doc450021022_620099497")
             }
             startService(downloadIntent)
         }
@@ -103,6 +127,11 @@ class MainActivity : AppCompatActivity(), ServiceCallbacks {
                 append("Влажность: ${main.humidity}")
             }
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(downloadReceiver)
+        super.onDestroy()
     }
 
 }
